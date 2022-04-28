b459aac3ad5d8b0f21949a740d8e86d83eceb181
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 13:53:11 +0700

    Percobaan 14 mengimplementasikan multiple interface(ini merupakan bagian kelas N_FirstInterface pada package multiple_interface)

---
+++ src/multiple_interface/N_FirstInterface.java
@@ -0,0 +1,14 @@
+/* Percobaan mengimplementasikan multiple interface
+(ini merupakan bagian kelas N_FirstInterface
+pada package multiple_interface)
+ */
+
+package multiple_interface;
+
+public interface N_FirstInterface {
+
+    int NUMBER= 13;
+    void printHello();
+    int doubleNumberA();
+
+}
