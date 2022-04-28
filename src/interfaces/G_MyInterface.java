3f6dec69e8cd0ade37ac3f0e4f26159d0c8bc5af
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 13:40:49 +0700

    Percobaan 7 mengimplementasikan single interface (ini merupakan bagian kelas G_MyInterface pada package interfaces

---
+++ src/interfaces/G_MyInterface.java
@@ -0,0 +1,18 @@
+/* Percobaan mengimplementasikan
+single interface (ini merupakan bagian kelas G_MyInterface
+pada package interfaces
+ */
+
+package interfaces;
+
+public interface G_MyInterface {
+
+    static final String GREETING  = "Hello  Interfaces";
+
+    int NUMBER= 91;
+
+    abstract void demo();
+
+    void printHello();
+
+}
