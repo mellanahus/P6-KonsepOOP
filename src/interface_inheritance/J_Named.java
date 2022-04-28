857dc791f44e0ed283eda52687525fbcd6957351
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 13:46:14 +0700

    Percobaan 10 mengimplementasikan interface inheritance(ini merupakan bagian kelas J_Named pada package interface_inheritance

---
+++ src/interface_inheritance/J_Named.java
@@ -0,0 +1,10 @@
+/* Percobaan mengimplementasikan interface inheritance
+(ini merupakan bagian kelas J_Named pada
+package interface_inheritance
+ */
+
+package interface_inheritance;
+
+public interface J_Named {
+    String getName();
+}
