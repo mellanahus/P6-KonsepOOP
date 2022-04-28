1c1752642caf7a93006c6acbaf486a1bc9b8fe65
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 13:54:19 +0700

    Percobaan 15 mengimplementasikan multiple interface(ini merupakan bagian kelas O_SecondInterface pada package multiple_interface)

---
+++ src/multiple_interface/O_SecondInterface.java
@@ -0,0 +1,14 @@
+/* Percobaan mengimplementasikan multiple interface
+(ini merupakan bagian kelas O_SecondInterface
+pada package multiple_interface)
+ */
+
+package multiple_interface;
+
+public interface O_SecondInterface {
+
+    int NUMBER= 11;
+    void printHello();
+    int doubleNumberB();
+
+}
