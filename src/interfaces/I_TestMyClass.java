b773862390c796756c7f501802a6604ac05cba33
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 13:43:48 +0700

    Percobaan 9 menjalankan single interface (ini merupakan bagian kelas I_TestMyClass pada package interfaces

---
+++ src/interfaces/I_TestMyClass.java
@@ -0,0 +1,15 @@
+/* Percobaan menjalankan
+single interface (ini merupakan bagian kelas I_TestMyClass
+pada package interfaces
+ */
+
+package interfaces;
+
+public class I_TestMyClass {
+    public static void main(String[] args) {
+
+        H_MyClass myClass = new H_MyClass();
+        myClass.demo();
+        myClass.printHello();
+    }
+}
