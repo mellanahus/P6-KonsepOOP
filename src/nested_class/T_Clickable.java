5f9cc0b88dab69ce8defc5919ec4d133a9272261
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 14:03:20 +0700

    Percobaan 20 membuat anonymous inner class(ini merupakan bagian kelas T_Clickable pada package nested_class)

---
+++ src/nested_class/T_Clickable.java
@@ -0,0 +1,17 @@
+/* Percobaan membuat anonymous inner class
+(ini merupakan bagian kelas T_Clickable
+pada package nested_class)
+ */
+
+package nested_class;
+
+public class T_Clickable {
+
+    public void onClick() {
+    }
+
+    interface Clickable {
+        void onClick();
+    }
+
+}
